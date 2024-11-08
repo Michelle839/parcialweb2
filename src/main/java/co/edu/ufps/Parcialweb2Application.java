2471222d8d4ee41c2c4ab964737b8713a7adf01e
Michelle839 <michellealexandrard@ufps.edu.co>
2024-11-08 10:20:39 -0500

    Proyecto base

---
+++ src/main/java/co/edu/ufps/Parcialweb2Application.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcialweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcialweb2Application.class, args);
+	}
+
+}
